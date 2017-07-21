27798dcab38afb183665b5ae6fced02398527af5
Franco Arolfo <francoarolfo@hotmail.com>
2017-07-21 03:25:56 -0300

    Added D3 and pixel title

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Run
 
+```
 browserify app.js -o bundle.js && python -m SimpleHTTPServer 8000
+```
--- app.js
+++ app.js
@@ -18,8 +18,10 @@ function startApp() {
   const eth = new Eth(web3.currentProvider);
   const ethContract = new EthContract(eth);
 
-  initWindow();
   initContract(ethContract);
+  initWindow();
+
+  setInterval(refreshWindow, 1000);
 }
 
 const abi = [
@@ -67,10 +69,6 @@ const abi = [
         {
           "name": "color",
           "type": "string"
-        },
-        {
-          "name": "price",
-          "type": "uint256"
         }
       ],
       "name": "buyPixel",
@@ -80,57 +78,72 @@ const abi = [
     }
   ];
 
-const address = '0x341fb0cef01bdaec38771442b6d18a21b45bbbb2';
+const address = '0xbca6fa8b17f355a92b2c272d6403185f433ab8db';
 
 const WINDOW_SIZE = 4;
 const PIXEL_SIZE = 20;
 
 var contract;
 
-var _x, _y;
-
 function initWindow() {
-    var root = $('#window');
-
-    root.css('width', (WINDOW_SIZE*PIXEL_SIZE) + 'px');
-    root.css('height', (WINDOW_SIZE*PIXEL_SIZE) + 'px');
-
-    for (var x = 0; x < WINDOW_SIZE; x++) {
-        for (var y = 0; y < WINDOW_SIZE; y++) {
-            root.append(createPixel(x, y));
-        }
-    }
+    d3.select('#window').append('svg')
+        .attr('height', WINDOW_SIZE*PIXEL_SIZE)
+        .attr('width', WINDOW_SIZE*PIXEL_SIZE)
+        .selectAll('rect')
+            .data(getInitialEmptyWindowGrid())
+        	.enter()
+            .append('rect')
+            .attr('x', d => d.x * PIXEL_SIZE)
+        	.attr('y', d => d.y * PIXEL_SIZE)
+        	.attr('width', PIXEL_SIZE)
+            .attr('height', PIXEL_SIZE)
+            .attr('id', d => 'pixel-' + d.x + "-" + d.y)
+            .attr('onclick', d => 'openBuyPixelModal(' + d.x + ',' + d.y + ');')
+            .style('fill', 'black');
 }
 
-function createPixel(x, y) {
-    return $('<div></div>')
-              .addClass('pixel')
-              .css('width', PIXEL_SIZE + 'px')
-              .css('height', PIXEL_SIZE + 'px')
-              .css('top', (x*PIXEL_SIZE) + 'px')
-              .css('left', (y*PIXEL_SIZE) + 'px')
-              .attr('id', 'pixel-' + x + "-" + y)
-              .attr('onclick', 'openBuyPixelModal(' + x + ',' + y + ');')
-              .tooltip({
-                title: 'Pixel (' + x + ',' + y + '): It is free!'
-              });
+function getInitialEmptyWindowGrid() {
+    var resp = [];
+    for (var x = 0; x < WINDOW_SIZE; x++) {
+        for (var y = 0; y < WINDOW_SIZE; y++) {
+            resp.push({x: x, y: y});
+        }
+    }
+    return resp;
 }
 
 function initContract(ethContract) {
   const EthMillonDollarHomepage = ethContract(abi);
   contract = EthMillonDollarHomepage.at(address);
-
-  setInterval(updateWindow, 1000);
 }
 
-function updateWindow() {
-    for (var x = 0; x < WINDOW_SIZE; x++) {
-        for (var y = 0; y < WINDOW_SIZE; y++) {
-            console.log('---' + x + ',' + y);
-            getPixel(x, y).then(p => updatePixel(p, x, y));
-        }
-    }
+function refreshWindow() {
+//    for (var x = 0; x < WINDOW_SIZE; x++) {
+//        for (var y = 0; y < WINDOW_SIZE; y++) {
+//            getPixel(x, y).then(p => updatePixel(p, x, y));
+//        }
+//    }
+
+    // ^^
+    // The for loop doesn't works but this does, digging why
+    getPixel(0, 0).then(p => updatePixel(p, 0, 0));
+    getPixel(0, 1).then(p => updatePixel(p, 0, 1));
+    getPixel(0, 2).then(p => updatePixel(p, 0, 2));
+    getPixel(0, 3).then(p => updatePixel(p, 0, 3));
+    getPixel(1, 0).then(p => updatePixel(p, 1, 0));
+    getPixel(1, 1).then(p => updatePixel(p, 1, 1));
+    getPixel(1, 2).then(p => updatePixel(p, 1, 2));
+    getPixel(1, 3).then(p => updatePixel(p, 1, 3));
+    getPixel(2, 0).then(p => updatePixel(p, 2, 0));
+    getPixel(2, 1).then(p => updatePixel(p, 2, 1));
+    getPixel(2, 2).then(p => updatePixel(p, 2, 2));
+    getPixel(2, 3).then(p => updatePixel(p, 2, 3));
+    getPixel(3, 0).then(p => updatePixel(p, 3, 0));
+    getPixel(3, 1).then(p => updatePixel(p, 3, 1));
+    getPixel(3, 2).then(p => updatePixel(p, 3, 2));
+    getPixel(3, 3).then(p => updatePixel(p, 3, 3));
 }
+
 
 function getPixel(x, y) {
     return contract.checkPixel(x, y);
@@ -140,21 +153,24 @@ function buyPixel() {
     var price = $('#price').val();
     var color = $('#color').val();
 
-    return contract.buyPixel(_x, _y, color, parseInt(price), {
+    return contract.buyPixel(targetX, targetY, color, {
         from: web3.eth.accounts[0],
         value: web3.toWei(price, 'ether')
     });
 }
 
+var targetX, targetY;
+
 function openBuyPixelModal(x, y) {
-    _x = x;
-    _y = y;
+    targetX = x;
+    targetY = y;
+    $('#targetPixel').text('(' + x + ',' + y + ')');
     $('#buyPixelModal').modal('show');
 }
 
 function updatePixel(pixel, x, y) {
     console.log('Pixel at (' + x + ',' + y + ') has color ' + pixel.color);
-    $('#pixel-' + x + '-' + y).css('background-color', pixel.color ? pixel.color : 'black');
+    d3.select('#pixel-' + x + '-' + y).style('fill', pixel.color ? pixel.color : 'black');
 }
 
 window.openBuyPixelModal = openBuyPixelModal;
